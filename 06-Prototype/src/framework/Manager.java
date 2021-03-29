3101bc977c71cd74c638a1fa6069c5c940588d17
LeneyHomon <leneyhomon@gmail.com>
2021-03-29 22:33:03 +0800

    chapter 06 Prototype

---
+++ 06-Prototype/src/framework/Manager.java
@@ -0,0 +1,16 @@
+package framework;
+
+import java.util.HashMap;
+
+public class Manager {
+    private HashMap<String, Product> showcase = new HashMap<>();
+
+    public void register(String name, Product product) {
+        showcase.put(name, product);
+    }
+
+    public Product create(String name) {
+        Product p = (Product) showcase.get(name);
+        return p.createClone();
+    }
+}
